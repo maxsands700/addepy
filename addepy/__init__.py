e002b83688e16faca47613cd3c3142267a8e08bc
maxsands700 <maxsands700@gmail.com>
2025-12-12 14:40:36 -0500

    chore: release v0.3.0

--- addepy/__init__.py
+++ addepy/__init__.py
@@ -38,7 +38,7 @@ from .exceptions import (
 logger = logging.getLogger("addepy")
 logger.addHandler(logging.NullHandler())  # Prevent "no handler" warnings
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "AddePy",
